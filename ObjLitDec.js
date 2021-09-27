8ae6321f3a08232afccd83500b3e659d23ba3136
hyun5292 <57443458+hyun5292@users.noreply.github.com>
2021-09-27 16:23:19 +0900

    Object Literal Declarations
    객체 속성 약식을 사용하여 간결한 객체 리터럴 선언 작성?

---
+++ ObjLitDec.js
@@ -0,0 +1,34 @@
+////Write Concise(간결한) Object Literal Declarations Using Object Property Shorthand(개체 속성 약식?)
+/*
+ES6 adds some nice support for easily defining object literals.
+
+Consider the following code:
+*/
+/*
+const getMousePosition = (x, y) => ({
+  x: x,
+  y: y
+});
+*/
+/*
+getMousePosition is a simple function that returns an object containing two properties.
+ES6 provides the syntactic sugar to eliminate the redundancy of having to write x: x.
+You can simply write x onec, and it will be converted to x: x (or something equivalent)
+under the hood. Here is the same function from above rewritten to use this new syntax:
+*/
+//const getMousePosition = ( x, y ) => ({ x, y });
+////////////////////////////////////////////////////////////////////////////////
+/*
+Use object property shorthand with object literals to create and return an object
+with name, age and gender properties.
+*/
+/*
+const createPerson = (name, age, gender) => {
+  return {
+    //name: name,
+    //age: age,
+    //gender: gender
+    name, age, gender
+  };
+};
+*/
