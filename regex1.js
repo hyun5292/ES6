1e56997612d6dbe24b35696e3bcb124ff751d7eb
hyun5292 <57443458+hyun5292@users.noreply.github.com>
2021-09-28 15:31:37 +0900

    Update regex1.js

--- regex1.js
+++ regex1.js
@@ -116,7 +116,9 @@ should not match any abbrevaiations(약어) or variations(변형) with spaces.
 let myString = "freeCodeCamp";
 let fccRegex = /freecodecamp/i;
 let result = fccRegex.test(myString);
-*/////Extract Matches
+*/
+////////////////////////////////////////////////////////////////////////////////
+////Extract Matches
 /*
 So far, you have only been checking if a pattern exists or not within a string.
 You can also extract the actual matches you found with the .match() method.
@@ -213,5 +215,45 @@ and bun. Your regex should use the wildcard character.
 let exampleStr = "Let's have fun with regular expressions!";
 let unRegex = /.un/;
 let result = unRegex.test(exampleStr);
+*/
+////////////////////////////////////////////////////////////////////////////////
+////Match Single Character with Multiple Possibilities
+/*
+You learned how to match literal patterns (/literal/) and wildcard character (/./).
+Those are the extremes of regular expressions, where one finds exact mathes and the
+other matches everything. There are options that are a balance between the two extremes.
+
+You can search for a literal pattern with some flexibility with character classes.
+Character classes allow you to define a group of characters you wish to match
+by placing them inside square ([ and ]) brackets.
+
+For example, you want to match bag, big and bug but not bog. You can create the
+regex /b[aiu]g/ to do this. The [aiu] is the character class that will only
+match the characters a, i or u.
+*/
+/*
+let bigStr = "big";
+let bagStr = "bag";
+let bugStr = "bug";
+let bogStr = "bog";
+let bgRegex = /b[aiu]g/;
+bigStr.match(bgRegex);
+bagStr.match(bgRegex);
+bugStr.match(bgRegex);
+bogStr.match(bgRegex);
+*/
+/*
+In order, the four match calls would return the values ["big"], ["bag"], ["bug"]
+and null.
+*/
+////////////////////////////////////////////////////////////////////////////////
+/*
+Use a character class with vowels (a, e, i, o, u) in your regex vowelRegex to find
+all the vowels in the string quoteSample.
+*/
+/*
+let quoteSample = "Beware of bugs in the above code I have onlly proved it correct, not tried it.";
+let vowelRegex = /[aeiou]/gi;
+let result = quoteSample.match(vowelRegex);
 */
 ////////////////////////////////////////////////////////////////////////////////
