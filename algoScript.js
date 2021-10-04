13d3806b717710adb3ee7e511bfb097e5d80edda
hyun5292 <57443458+hyun5292@users.noreply.github.com>
2021-10-05 02:18:19 +0900

    JS Algorithms and Data Structure
    Convert Celsius to Fahrenheit
    Reverse a String
    Factorialize a Number
    Find the Longest Word in a String
    Return Largest Numbers in Arrays
    Confirm the Ending
    Repeat a string Repeat a String
    Finders Keepers
    Boo Who
    Title Case a Sentence -- 모르겠음!
    Slice and Splice
    Falsy Bouncer
    Where do I Belong
    Mutations
    Chunky Monkey

--- algoScript.js
+++ algoScript.js
@@ -194,12 +194,113 @@ sure the rest of the word is in lower case.
 For the purpose of this exercise, you should also capitalize connecting words like
 the and of.
 */
+/*
 function titleCase(str) {
-  let arr = str.split(" ");
-  let result = "";
-  for(let i = 0; i < arr.length; i++) {
-    arr[i]
-  }
-  return str;
+  let arr = str.toLowerCase().split(" ");
+  let result = arr.map(function(val) {
+    return val.replace(val.charAt(0), val.charAt(0).toUpperCase());
+  });
+  return result.join(" ");
 }
 titleCase("I'm a little tea pot");
+*/
+////////////////////////////////////////////////////////////////////////////////
+////Slice and Splice
+/*
+You are given two arrays and an index.
+Copy each elemeent of the first array into the second array, in order.
+Begin inserting elements at index n of the second array.
+Return the resulting array. The input arrays should remian the same after the
+function runs.
+*/
+/*
+function frankenSplice(arr1, arr2, n) {
+  let result = arr2.slice();
+  result.splice(n, 0, ...arr1);
+  return result;
+}
+console.log(frankenSplice([1, 2, 3], [4, 5, 6], 1));
+*/
+////////////////////////////////////////////////////////////////////////////////
+////Falsy Bouncer
+/*
+Remove all falsy values from an array.
+Falsy values in JavaScript are false, null, 0, "", undeinfed and NaN.
+*/
+/*
+function bouncer(arr) {
+  let result = [];
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i]) {
+      result.push(arr[i]);
+    }
+  }
+  return result;
+}
+console.log(bouncer([7, "ate", "", false, 9]));
+*/
+////////////////////////////////////////////////////////////////////////////////
+////Where do I Belong
+/*
+Return the lowest index at which a value (second argument) should be inserted into
+an array (first argument) once it has been sorted. The returned value should be a
+number.
+For example, getIndexToIns([1, 2, 3, 4], 1.5) should return 1 because it is greater
+than 1 (index 0), but less than 2 (index 1).
+Likewise, getIndexToIns([20, 3, 5], 19) should return 2 because once the array
+has been sorted it will look like [3, 5, 20] and 19 is less than 20 (index 2) and
+greater than 5 (index 1).
+*/
+/*
+function getIndexToIns(arr, num) {
+  arr.sort((a, b) => a - b);
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] >= num) {
+      return i;
+    }
+  }
+  return arr.length;
+}
+console.log(getIndexToIns([40, 60], 50));
+*/
+////////////////////////////////////////////////////////////////////////////////
+////Mutations
+/*
+Return true if the string in the first element of the array contains all of the
+letters of the string in the second element of the array.
+For example, ["hello", "Hello"], should return true because all of the letters in
+the second string are present in the first, ignoring case.
+The arguments ["hello", "hey"] should return false because the string hello does
+not contain y.
+Lastly, ["Alien", "line"], should return true because all of the letters in line
+are present in Alien.
+*/
+/*
+function mutation(arr) {
+  let test = arr[1].toLowerCase();
+  let target = arr[0].toLowerCase();
+  for (let i = 0; i < test.length; i++) {
+    if (target.indexOf(test[i]) < 0) return false;
+  }
+  return true;
+}
+mutation(["hello", "hey"]);
+*/
+////////////////////////////////////////////////////////////////////////////////
+////Chunky Monkey
+/*
+Write a function that splits an array (first argument) into groups the length of
+size (second argument) and returns them as a two-dimensional array.
+*/
+/*
+function chunkArrayInGroups(arr, size) {
+  let result = [];
+  for(let i = 0; i < arr.length; i+=size) {
+    result.push(arr.slice(i, i+size));
+  }
+  return result;
+}
+console.log(chunkArrayInGroups(["a", "b", "c", "d", "e"], 2));
+*/
+////////////////////////////////////////////////////////////////////////////////
