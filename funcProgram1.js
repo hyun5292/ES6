d2f0545c8657e32b924293a93701aec176e4f9f8
hyun5292 <57443458+hyun5292@users.noreply.github.com>
2021-10-06 16:27:06 +0900

    Functional Programming 1

--- funcProgram1.js
+++ funcProgram1.js
@@ -199,3 +199,131 @@ var finalTabs = socialWindow
 console.log(finalTabs.tabs);
 */
 ////////////////////////////////////////////////////////////////////////////////
+////Avoid Mutations and Side Effects Using Functional Programming
+/*
+If you haven't already figured it out, the issue in the previous challenge was with
+the splice call in the tabClose() funciton. Unfortunately, splice changes the
+original array it is called on, so the second call to it used a modified array,
+and gave unexpected results.
+
+This is a small example of much larger pattern - you call a function on a variable,
+array or an object, and the function changes the variable or something in the object.
+
+One of the core principles of functional programming is to not change things. Changes
+lead to bugs. It's easier to prevent bugs knowing that your functions don't change
+anything, including the function arguments or any global variable.
+
+The previous example didn't have any complicated operations but the splice method
+changed the orginal array, and resulted in a bug.
+
+Recall that in functional programming, changing or altering things is called mutation,
+and the outcome is called a side effect. A function, ideally, should be a pure function,
+meaning that it does not cause any side effects.
+
+Let's try to master this discipline(규율) and not alter any variable or object in
+our code.
+*/
+////////////////////////////////////////////////////////////////////////////////
+/*
+Fill in the code for the function incrementer so it returns the value of the global
+variable fixedValue increased by one.
+*/
+/*
+var fixedValue = 4;  //The global variable
+
+function incrementer() {
+  let result = fixedValue;
+  return ++result;
+}
+*/
+////////////////////////////////////////////////////////////////////////////////
+////Pass Arguments to Avoid External Dependence in a Function
+/*
+The last challenge was a step closer to functional programming principles, but
+there is still something missing.
+
+We didn't alter the global varaible value, but the function incrementer would not
+work without the global variable fixedValue being there.
+
+Another principle of functional programming is to always declare your dependencies
+explicity. This means if a function depends on a variable or object being present,
+then pass that variable or obejct directly into the function as an argument.
+
+There are several good consequences from this principle. The function is easier
+to test, you know exactly what input it takes, and it won't depend on anything
+else in your program.
+
+This can give you more confidence when you alter, remove or add new code. You
+would know what you can or cannot change and you can see where the potential traps
+are.
+
+Finally, the function would always produce the same output for the same set of
+inputs, no matter what part of the code executes it.
+*/
+////////////////////////////////////////////////////////////////////////////////
+/*
+Let's update the incrementer function to clearly declare its dependencies.
+
+Write the incrementer function so it takes an argument, and then returns a result
+after increasing the value by one.
+*/
+/*
+var fixedValue = 4;  //The global variable
+
+function incrementer(arg) {
+  let result = arg+1;
+  return result;
+}
+*/
+////////////////////////////////////////////////////////////////////////////////
+////Refactor Global Variable Out of Functions
+/*
+So far, we have seen two distinct principles for functional programming:
+
+1. Don't alter a variable or object - create new variables and objects and return
+   them if need be from a function. Hint: using something like var newArr = arrVar,
+   where arrVar is an array will simply create a reference to the existing varaible
+   and not a copy. So changing a value in newArr would change the value in arrVar.
+2. Declare function parameters - any computation inside a function depends only
+   on the arguments passed to the function, and not on any global object or variable.
+
+Adding one to a number is not very exciting, but we can apply these principles
+when working with arrays or more complex objects.
+*/
+////////////////////////////////////////////////////////////////////////////////
+/*
+Rewrite the code so the global array bookList is not changed inside either function.
+The add function should add the given bookName to the end of the array passed to
+it and return a new array (list). The remove function should remove the given
+bookName from the array passed to it.
+
+Note: Both functions should return an array, and any new parameters should be
+added before the bookName parameter.
+*/
+/*
+//The global variable
+var bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies",
+"Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
+
+function add (list, bookName) {
+  let newArr = list.slice();
+  newArr.push(bookName);
+  return newArr;
+}
+
+function remove (list, bookName) {
+  let newArr = list.slice();
+  var book_index = newArr.indexOf(bookName);
+  if (book_index >= 0) {
+    newArr.splice(book_index, 1);
+    return newArr;
+  }
+}
+
+var newBookList = add(bookList, 'A Brief History of Time');
+var newerBookList = remove(bookList, 'On The Electrodynamics of Moving Bodies');
+var newestBookList = remove(add(bookList, 'A Brief History of Time'), 'On the Electrodynamics of Moving Bodies');
+
+console.log(bookList);
+*/
+////////////////////////////////////////////////////////////////////////////////
