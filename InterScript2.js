e18b484c37c7f15c7e43f6214e22f3d2b1b75b90
hyun5292 <57443458+hyun5292@users.noreply.github.com>
2021-10-08 23:22:20 +0900

    Intermediate Algorithm Scripting 2
    skill 테스트 및 다른 사람들은 어떻게 했나 공부하니 재밌구만
    - 재공부 필요

--- InterScript2.js
+++ InterScript2.js
@@ -1,24 +1,79 @@
-////Search and Replace
+////DNA Pairing
 /*
-Perform a search and replace on the sentence using the arguments provided and return
-the new sentence.
-First argument is the sentence to perform the search and replace on.
-Second argument is the word that you will be replacing (before).
-Third argument is what you will be replacing the second argument with (after).
-
-Note: Preserve the case of the first character in the original word when you are
-replacing it. For example if you mean to replace the word Book with the word dog,
-it should be replaced as Dog
+The DNA strand is missing the pairing element. Take each character, get its pair,
+and return the results as a 2d array.
+Base pairs are a pair of AT and CG. Match the missing element to the provided
+character.
+Return the provided character as the first element in each array.
+For example, for the input GCG, return [["G", "C"], ["C","G"], ["G", "C"]]
+The character and its pair are paired up in an array, and all the arrays are
+grouped into one encapsulating array.
 */
-//내소스 - 캬캬 쉽게 했다
+//내소스 - AT, GC, TA, CG 이 4가지로 이루어진 걸 말하는 듯
 /*
-function myReplace(str, before, after) {
-  var result = "";
-  (/^[A-Z]/).test(before)
-    ? result = str.replace(before, after[0].toUpperCase() + after.substr(1, after.length))
-    : result = str.replace(before, after[0].toLowerCase() + after.substr(1, after.length));
-
+function pairElement(str) {
+  var strArr = str.split("");
+  var result = [];
+  for(let i = 0; i < strArr.length; i++) {
+    switch(strArr[i]) {
+      case 'G':
+        result.push(['G', 'C']);
+        break;
+      case 'C':
+        result.push(['C', 'G']);
+        break;
+      case 'A':
+        result.push(['A', 'T']);
+        break;
+      case 'T':
+        result.push(['T', 'A']);
+        break;
+    }
+  }
   return result;
 }
 */
-//
+//솔루션 1 - 나랑 똑같은데 for문 안에 switch를 밖으로 꺼낸 경우
+/*
+function pairElement(str) {
+  var paired = [];
+  var search = function(char) {
+    switch(char) {
+      case "A":
+        paired.push(["A", "T"]);
+        break;
+      case "T":
+        paired.push(["T", "A"]);
+        break;
+      case "C":
+        paired.push(["C", "G"]);
+        break;
+      case "G":
+        paired.push(["G", "C"]);
+        break;
+    }
+  };
+
+  for(var i = 0; i < str.length; i++) {
+    search(str[i]);
+  }
+
+  return paired;
+}
+*/
+//솔루션 2
+/*
+function pairElement(str) {
+  var pairs = {  //오 객체로
+    A: "T",
+    T: "A",
+    C: "G",
+    G: "C"
+  };
+
+  var arr = str.split("");
+  //공부 메모에 배열용 반복문이라 되어있네
+  return arr.map(x => [x, pairs[x]]);  //오 근데 어뜨케 이 한 문장으로 되는거지ㅋㅋ
+}
+*/
+////////////////////////////////////////////////////////////////////////////////
