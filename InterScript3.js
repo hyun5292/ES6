916e2a921a396cc701355295e961da8155556004
hyun5292 <57443458+hyun5292@users.noreply.github.com>
2021-10-12 02:48:09 +0900

    Intermediate Algorithm Scripting 3
    skill 테스트 및 다른 사람들은 어떻게 했나 공부하니 재밌구만
    - 재공부 필요
    Steamroller - 솔루션5

--- InterScript3.js
+++ InterScript3.js
@@ -60,4 +60,77 @@ function dropElements(arr, func, i = 0) {
 }
 */
 ////////////////////////////////////////////////////////////////////////////////
-////
+////steamroller
+/*
+Flatten a nested array. You must account for varying levels of nesting.
+*/
+//내소스 - 줄였다!! 드디어 줄였드아아아!!
+/*
+function steamrollArray(arr, result) {
+  if(result == null) result = [];
+  arr.map(a => Array.isArray(a) ? steamrollArray(a, result) : result.push(a));
+  return result;
+}
+*/
+//솔루션 1 - 하하 내가 줄이기 전에 이거랑 비슷했지 하하
+/*
+function steamrollArray(arr) {
+  const flattenedArray = [];
+  for(let i = 0; i < arr.length; i++) {
+    if(Array.isArray(arr[i])) {
+      flattenedArray.push(...steamrollerArray(arr[i]));
+    } else {
+      flattenedArray.push(arr[i]);
+    }
+  }
+  return flattenedArray;
+}
+*/
+//솔루션 2 - .some()...좋군 역시 함수 안에 함수구나
+/*
+function steamrollArray(arr) {
+  const flat = [].concat(...arr);
+  //배열 안의 어떤 요소라도 주어진 판별 함수를 통과하는지 테스트
+  return flat.some(Array.isArray) ? steamrollArray(flat) : flat;
+}
+*/
+//솔루션 3 - 내가 하다가 막혔던 것을 조건으로 해결했구나 그래도 되는구낭
+/*
+function steamrollArray(arr) {
+  return arr
+    .toString()  //a,b
+    .replace(",,", ",")  //1,2,,3 -> 1,2,3
+    .split(",")
+    .map(function(v) {
+      if(v == "[object object]") {  //이거 예시 [1, [], [3, [[4]]]]에서 경험해따...
+        return {};
+      } else if(isNaN(v)) {
+        return v;
+      } else {
+        return parseInt(v);  //만약 숫자인데 string된거면 다시 숫자로
+      }
+    });
+}
+*/
+//솔루션 4 - 아 매개변수에서 =[]로 초기화해도 값이 전달되면 유지되는구나
+/*
+function steamrollArray(val,flatArr=[]) {
+  val.forEach(item => {
+    if(Array.isArray(item)) steamrollArray(item, flatArr);
+    else flatArr.push(item);
+  });
+  return flatArr;
+}
+*/
+//솔루션 5 - 크으으 다들 Array.isArray() 썼구나 인터넷에서 잘 찾았군 배열 깊이 검색하다가 나왔는데
+//근데 !Array.isArray(elem) 왜 이거 앞에 !가 붙은건지 더 고민해봐야할듯
+function steamrollArray(arr, flatArr = []) {
+  const elem = arr.pop(); //.pop()이 배열 맨 뒤 추출?, [1, [2], [3, [[4]]]]->[ 3, [ [ 4 ] ] ]
+  console.log(elem);
+  return elem
+    ? !Array.isArray(elem)  //배열인가 아닌가
+      ? steamrollArray(arr, [elem, ...flatArr])  //배열이 아니면
+      : steamrollArray(arr.concat(elem), flatArr)
+    : flatArr;  //배열이면
+}
+console.log(steamrollArray([1, [2], [3, [[4]]]]));
